7fa366df67d2e0d675a978ac3afa3cd27ad8cef5
sirteddanson <knowledgeMED@gmail.com>
2021-12-22 17:20:56 -0500

    added when

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const { homedir } = require('os');
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
@@ -40,9 +41,22 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message:  'Would you like to enter some information about yourself fo an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
